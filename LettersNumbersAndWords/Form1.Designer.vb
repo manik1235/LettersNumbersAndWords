a5d4d86747e54e0c4a9d6b10f255548b369c7d35
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2017-10-01 11:29:37 -0400

    words are said, but the speaking of letters and words aren't in the right order

--- LettersNumbersAndWords/Form1.Designer.vb
+++ LettersNumbersAndWords/Form1.Designer.vb
@@ -22,32 +22,68 @@ Partial Class MainForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.LetterPanel = New System.Windows.Forms.Panel()
         Me.LetterLabel = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.LetterPanel.SuspendLayout()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'LetterPanel
+        '
+        Me.LetterPanel.Controls.Add(Me.LetterLabel)
+        Me.LetterPanel.Location = New System.Drawing.Point(12, 12)
+        Me.LetterPanel.Name = "LetterPanel"
+        Me.LetterPanel.Size = New System.Drawing.Size(187, 150)
+        Me.LetterPanel.TabIndex = 1
         '
         'LetterLabel
         '
         Me.LetterLabel.AutoSize = True
         Me.LetterLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LetterLabel.Location = New System.Drawing.Point(203, 74)
+        Me.LetterLabel.Location = New System.Drawing.Point(29, 27)
         Me.LetterLabel.Name = "LetterLabel"
         Me.LetterLabel.Size = New System.Drawing.Size(110, 108)
-        Me.LetterLabel.TabIndex = 0
+        Me.LetterLabel.TabIndex = 1
         Me.LetterLabel.Text = "E"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 168)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(850, 327)
+        Me.Panel1.TabIndex = 2
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(22, 23)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(807, 291)
+        Me.FlowLayoutPanel1.TabIndex = 0
         '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(554, 288)
-        Me.Controls.Add(Me.LetterLabel)
+        Me.ClientSize = New System.Drawing.Size(1086, 515)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.LetterPanel)
         Me.KeyPreview = True
         Me.Name = "MainForm"
         Me.Text = "Form1"
+        Me.LetterPanel.ResumeLayout(False)
+        Me.LetterPanel.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents LetterPanel As Panel
     Friend WithEvents LetterLabel As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
 End Class
--- LettersNumbersAndWords/Form1.vb
+++ LettersNumbersAndWords/Form1.vb
@@ -4,6 +4,9 @@ Option Strict On
 
 Public Class MainForm
 
+    Dim SentanceString As String = ""
+
+
     ''' <summary>
     '''   When a letter key is pressed, change the displayed letter
     ''' </summary>
@@ -12,26 +15,114 @@ Public Class MainForm
     Private Sub MainForm_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
         ChangeLetterOrColor(e)
 
+        AddNewLetterToFlow(e, LetterLabel)
+
+        PlayLetterAudio(e)
+
+
+
+    End Sub
+
+
+    ''' <summary>
+    '''   Add a letter label to the flow control. Adds in the current color and other properties
+    ''' </summary>
+    ''' <param name="e"></param>
+    ''' <param name="addLabel">The Label to add to the Flow Control.</param>
+    Private Sub AddNewLetterToFlow(e As KeyEventArgs, ByVal addLabel As Label)
+        ' Use overloads to handle other types?
+
+        Dim newLabel As New Label
+
+        With addLabel
+            ' Copy over the relavent properties
+            newLabel.Text = .Text
+            newLabel.Font = .Font
+            newLabel.ForeColor = .ForeColor
+            newLabel.BackColor = .BackColor
+            newLabel.Size = .Size
+        End With
+
+        ' Add the new letter to the sentance string as well
+        AddNewLetterToSentance(newLabel.Text)
+
+
+        FlowLayoutPanel1.Controls.Add(newLabel)
+
+
+    End Sub
+
+    ''' <summary>
+    '''   Adds the new letter to the sentance string, and checks to see if it is a word that should be played
+    ''' </summary>
+    ''' <param name="text"></param>
+    Private Sub AddNewLetterToSentance(text As String)
+        SentanceString &= text
+
+        dim ILoveArthurString As String = "I LOVE ARTHUR"
+        Dim IString As String = "I"
+        Dim LoveString As String = "LOVE"
+        Dim ArthurString As String = "ARTHUR"
+
+
+        If (Strings.Right(SentanceString, Len(LoveString)) = LoveString) Then
+            'The right most part says "I"
+            PlayWordAudio(LoveString)
+        End If
+
+    End Sub
+
+    Private Shared Sub PlayWordAudio(PlayWord As String)
         My.Computer.Audio.Stop()
 
-        Dim PlayStringBase As String
+        Dim PlayStringBase As String = "C:\Users\andre\Documents\Visual Studio 2017\Projects\LettersNumbersAndWords\LettersNumbersAndWords\Assets\Audio\Words\"
         Dim PlayStringName As String
         Dim PlayStringExtension As String = ".wav"
         Dim PlayStringFull As String
 
-        PlayStringBase = "C:\Users\andre\Documents\Visual Studio 2017\Projects\LettersNumbersAndWords\LettersNumbersAndWords\Assets\Audio\"
+        PlayStringName = PlayWord
+
+        PlayStringFull = PlayStringBase & PlayStringName & PlayStringExtension
+
+        Try
+            My.Computer.Audio.Play(PlayStringFull, AudioPlayMode.Background)
+        Catch ex As Exception
+
+            Debug.Print("PlayWordAudio Exception: " & ex.Message)
+
+            'Stop
+        End Try
+        Stop
+    End Sub
+
+
+
+    Private Shared Sub PlayLetterAudio(e As KeyEventArgs)
+        My.Computer.Audio.Stop()
+
+        Dim PlayStringBase As String = "C:\Users\andre\Documents\Visual Studio 2017\Projects\LettersNumbersAndWords\LettersNumbersAndWords\Assets\Audio\"
+        Dim PlayStringName As String
+        Dim PlayStringExtension As String = ".wav"
+        Dim PlayStringFull As String
 
         PlayStringName = StrConv(Chr(e.KeyCode), VbStrConv.Uppercase)
 
         PlayStringFull = PlayStringBase & PlayStringName & PlayStringExtension
 
-        My.Computer.Audio.Play(PlayStringFull, AudioPlayMode.Background)
+        Try
+            My.Computer.Audio.Play(PlayStringFull, AudioPlayMode.Background)
+        Catch ex As Exception
+
+            Debug.Print("PlayLetterAudio Exception: " & ex.Message)
+
+            'Stop
+        End Try
 
     End Sub
 
     Private Sub ChangeLetterOrColor(e As KeyEventArgs)
-        If (e.KeyCode >= Keys.A And e.KeyCode <= Keys.Z) Or (e.KeyCode >= Keys.D0 And e.KeyCode <= Keys.D9) Then
-            ' If it's a letter or a number, and the label isn't currenlty displaying it, update the lable
+        If (e.KeyCode >= Keys.A And e.KeyCode <= Keys.Z) Or (e.KeyCode >= Keys.D0 And e.KeyCode <= Keys.D9) Or (e.KeyCode = Keys.Space) Then
+            ' If it's a letter or a number or a space, and the label isn't currenlty displaying it, update the label
             If ((StrConv(Chr(e.KeyCode), VbStrConv.Uppercase)) <> LetterLabel.Text) Then
                 ' If the letter is different than the label, update it.
                 LetterLabel.Text = StrConv(Chr(e.KeyCode), VbStrConv.Uppercase)
